8a939674bc924515f06f4c7283ef763dd01aa5a1
jennifer.dai@berkeley.edu <jennifer.dai@berkeley.edu>
2015-04-29 17:41:51 -0700

    dummy plot working smap

--- ricecooker.py
+++ ricecooker.py
@@ -3,29 +3,36 @@ import urllib2
 import sys
 import msgpack
 import socket
+from smap.archiver.client import RepublishClient
 from smap import driver, util
 
 
-class ledApp(driver.SmapDriver):
+class riceCookerApp(driver.SmapDriver):
 
     def setup(self, opts):
         self.rate = float(opts.get('rate', 1))
         self.add_timeseries('/temp', 'unit', data_type='double')
-        self.set_metadata('/rice_app', {
+        self.set_metadata('/temp', {
                           'Metadata/Description' : 'Application for rice cooker',
                           })
 
-        self.archiverurl = opts.get('archiverurl','http://shell.storm.pm:8079')            
+        self.archiverurl = opts.get('archiverurl','http://shell.storm.pm:8079')                       
         self.subscription = opts.get('subscription','Metadata/SourceName = "Rice Cooker Sensor"')
-        self.r = RepublishClient(self.archiverurl, self.cb, restrict=self.subscription)    
+        self.r = RepublishClient(self.archiverurl, self.cb, restrict=self.subscription)               
 
     def cb(self, points):
         print points
-        curr_time = time.time()
-        self.add('/temp',curr_time, index)
+        #curr_time = int(time.time())
+        #self.add('/temp', curr_time)
 
     def start(self):
         self.r.connect()
+        util.periodicSequentialCall(self.dummyData).start(1)
+
+    def dummyData(self):
+        print "hello"
+        curr_time = int (time.time())
+        self.add("/temp", curr_time, float(2))
 
     def stop(self):
      print "Quit"
